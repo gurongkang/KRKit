77c03e7d461b1cfc9956ef708093c9125c2da5a2
gurongkang <1530669303@qq.com>
2018-10-16 20:32:14 +0800

    初始化登录项目

---
+++ KRLogin/KRLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KRLogin
+//
+//  Created by RK on 2018/10/16.
+//  Copyright © 2018年 RK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KRLogin/KRLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KRLogin
+//
+//  Created by RK on 2018/10/16.
+//  Copyright © 2018年 RK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
