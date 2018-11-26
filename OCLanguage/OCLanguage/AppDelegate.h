0980172d64df8937f0a091818e3eccb6d4adebff
gurongkang <1530669303@qq.com>
2018-11-26 20:47:18 +0800

    登录改成swift

---
+++ OCLanguage/OCLanguage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCLanguage
+//
+//  Created by RK on 2018/11/15.
+//  Copyright © 2018年 RK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCLanguage/OCLanguage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCLanguage
+//
+//  Created by RK on 2018/11/15.
+//  Copyright © 2018年 RK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
